810e9dce362f05dd8756472ccc00c4ba41152e26
Ariful0007 <arifulpub143@gmail.com>
2023-05-27 16:31:14 +0600

    llll

--- ImageFinal/ViewController.swift
+++ ImageFinal/ViewController.swift
@@ -178,11 +178,11 @@ class ViewController: UIViewController ,   CBCentralManagerDelegate, CBPeriphera
                                            printerCharacteristic = characteristic
                                            //image select
                                            
-                                          let newImage = convertImageToDifferentColorScale(with: UIImage(named: "man")!, imageStyle: "CIPhotoEffectNoir")
+                                          let newImage = convertImageToDifferentColorScale(with: UIImage(named: "sma")!, imageStyle: "CIPhotoEffectNoir")
                                            
                                            ///covertToGrayScale(with: newImage,cph: peripheral, charch: printerCharacteristic)
                                           /// print("char me "+printerCharacteristic.debugDescription)
-                                           covertToGrayScalezAngenl(with: newImage, cph: peripheral, charch: characteristic)
+                                           covertToGrayScalezAngenl2(with: newImage, cph: peripheral, charch: characteristic)
                                            
                                            var tttui = " \r\n\n\n"
                                                                       print(characteristic)
@@ -355,6 +355,10 @@ class ViewController: UIViewController ,   CBCentralManagerDelegate, CBPeriphera
                guard let context = CGContext(data: &pixels, width: width, height: height, bitsPerComponent: 8, bytesPerRow: width * 4, space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue | CGBitmapInfo.byteOrder32Little.rawValue) else {
                    return nil
                }
+        // Calculate the centered origin for drawing the image
+            let originX = (width - Int(image.size.width)) / 2
+            let originY = (height - Int(image.size.height)) / 2
+            
                
                context.draw(image.cgImage!, in: CGRect(x: 0, y: 0, width: width, height: height))
                
@@ -439,6 +443,107 @@ class ViewController: UIViewController ,   CBCentralManagerDelegate, CBPeriphera
               
             return commandData;
            }
+    func covertToGrayScalezAngenl2(with image: UIImage,  cph : CBPeripheral, charch : CBCharacteristic) -> Data? {
+              
+               let width = 255//Int(image.size.width)
+               let height = 255//Int(image.size.height)
+            print("good"+width.description+""+height.description)
+               // Pixels will be drawn in this array
+               var pixels = [UInt32](repeating: 0, count: width * height)
+               
+               let colorSpace = CGColorSpaceCreateDeviceRGB()
+               
+               // Create a context with pixels
+               guard let context = CGContext(data: &pixels, width: width, height: height, bitsPerComponent: 8, bytesPerRow: width * 4, space: colorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipLast.rawValue | CGBitmapInfo.byteOrder32Little.rawValue) else {
+                   return nil
+               }
+        // Calculate the centered origin for drawing the image
+            let originX = (width - Int(image.size.width)) / 2
+            let originY = (height - Int(image.size.height)) / 2
+            
+               
+               context.draw(image.cgImage!, in: CGRect(x: 70, y: 0, width: width, height: height))
+               
+               var tt = 1
+               var bw = 0
+               var bytes = [UInt8](repeating: 0, count: width / 8 * height)
+               var p = [Int](repeating: 0, count: 8)
+               
+               for y in 0..<height {
+                   for x in 0..<(width / 8) {
+                       for z in 0..<8 {
+                           let rgbaPixel = pixels[y * width + x * 8 + z]
+                           
+                           let red = (rgbaPixel >> 16) & 0xFF
+                           let green = (rgbaPixel >> 8) & 0xFF
+                           let blue = rgbaPixel & 0xFF
+                           let gray = 0.299 * Double(red) + 0.587 * Double(green) + 0.114 * Double(blue) // Grayscale conversion formula
+                           
+                           if gray <= 128 {
+                               p[z] = 0
+                           } else {
+                               p[z] = 1
+                           }
+                       }
+                       
+                       let value = p[0] * 128 + p[1] * 64 + p[2] * 32 + p[3] * 16 + p[4] * 8 + p[5] * 4 + p[6] * 2 + p[7]
+                       bytes[bw] = UInt8(value)
+                       bw += 1
+                   }
+               }
+            var commandData = Data()
+        
+        commandData.append(0x1D)
+        commandData.append(0x0E)
+        commandData.append(0x1C)
+        commandData.append(0x60)
+             
+        commandData.append(0x4D)
            
+        commandData.append(0x53)
+        let new_valesS = UInt8(3)
+        commandData.append(new_valesS)
+        commandData.append(0x1C)   //command use cpcl
+        commandData.append(0x60)
+        commandData.append(0x7E)
+        commandData.append(0x7E)
+        let new_valesSN = UInt8(16)
+        commandData.append(new_valesSN)
+        commandData.append(0x1D)
+        commandData.append(0x76)
+        commandData.append(0x30)
+        commandData.append(0x00)
+                let widthX  = 255
+                let heightX = 255
+                let widthL=widthX/8%256
+                let widthH=widthX/8/256
+                let heightL=heightX%256
+                let heightH=heightX/256
+                let valrues: [UInt8] = [1, 2, 3, 4]
+               let new_vales1 = UInt8(widthL)
+        commandData.append(new_vales1)
+                let new_vales2 = UInt8(widthH)
+        commandData.append(new_vales2)
+               let  new_vales3 = UInt8(heightL)
+        commandData.append(new_vales3)
+               let  new_vales4 = UInt8(heightH)
+        commandData.append(new_vales4)
+        
+        
+        
+        
+
+               commandData.append(contentsOf: bytes)
+        commandData.append(0x1C)
+        commandData.append(0x5E)
+        print(commandData)
+        //let data = Data(bytes: bytes)
+             // print("Good"+commandData.debugDescription)
+        //print("Getten ID : "+charch.debugDescription)
+            cph.writeValue(commandData, for: charch, type: .withoutResponse)
+        ////print(commandData)
+              
+            return commandData;
+           }
 }
 
